0940adf4411af8318eca2761721ebb858185549a
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-07-30 15:45:58 +0100

    Working on home screen

---
+++ components/onBoarding.jsx
@@ -0,0 +1,108 @@
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ArrowSmLeftIcon, ArrowSmRightIcon } from '@heroicons/react/outline'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from 'swiper';
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+export default function OnBoarding() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(false)}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end sm:items-center justify-center min-w-full p-4 text-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative bg-gray-100 px-4 pt-5 pb-4 text-left shadow-xl transform transition-all sm:my-8 lg:w-4/5 h-[35rem] sm:w-full sm:p-6">
+                <div className='flex justify-between items-center'>
+                  <img src='/stay.svg' alt='Stay SVG' className=''/>
+                  <button onClick={() => setIsOpen(false)} type='button' className='underline focus:outline-none text-indigo-900'>
+                    Skip
+                  </button>
+                </div>
+                <Swiper
+                  modules={[Pagination, Navigation]}
+                  navigation={true}
+                  grabCursor={true}
+                  pagination={{
+                    el: '.swiper__pagination',
+                    bulletClass: 'swiper__pagination__bullet',
+                    bulletActiveClass: 'swiper__pagination__bullet--active',
+                    type: "bullets",
+                    clickable: true,
+                    renderBullet: function (_index, _className) {
+                      return `<svg viewBox="0 0 10 10" class="swiper__pagination__bullet"><circle cx="5" cy="5" r="4.5"/></svg>`
+                    }
+                  }}
+                >
+                  <SwiperSlide className='text-center w-[50%] mx-auto'>
+                    <h3 className='text-lg text-indigo-900 font-bold'>Generate a personal address</h3>
+                    <p className='my-6 text-indigo-900'>
+                      When you generate a personal address a 
+                      wallet address is setup linked to the address
+                    </p>
+                    <img className='w-56 mx-auto' src='/carousel1.svg' alt='First slider image'/>
+                  </SwiperSlide>
+                  <SwiperSlide className='text-center w-[50%] mx-auto'>
+                    <h3 className='text-lg text-indigo-900 font-bold'>Send Crypto to address</h3>
+                    <p className='my-6 text-indigo-900'>
+                      After generating your address, you can now send any cryptocurrency to the address
+                    </p>
+                    <img className='w-56 mx-auto' src='/carousel2.svg' alt='First slider image'/>
+                  </SwiperSlide>
+                  <SwiperSlide className='text-center w-[50%] mx-auto'>
+                    <h3 className='text-lg text-indigo-900 font-bold'>Send cash to your bank account</h3>
+                    <p className='my-6 text-indigo-900'>
+                      You can fund your bank account from your wallet easily.
+                    </p>
+                    <img className='w-56 mx-auto' src='/carousel3.svg' alt='First slider image'/>
+                  </SwiperSlide>
+                  <div className='swiper__pagination'>
+                    
+                  </div>
+                </Swiper>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
+/*
+                    <button
+                      type="button"
+                      className="inline-flex items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                    >
+                      <ArrowSmLeftIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      <ArrowSmRightIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+*/
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import { EyeIcon, EyeOffIcon, ArrowNarrowUpIcon } from "@heroicons/react/solid";
+import { useState, Fragment } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import { EyeIcon, EyeOffIcon, XIcon, LibraryIcon, ArrowNarrowUpIcon } from "@heroicons/react/solid";
+
+import { banks } from "../utils/bankList";
+
+import ConfirmPayment from "../components/ConfirmPayment";
+import OnBoarding from "../components/onBoarding";
 
 export default function Home() {
   const [balanceVisibility, setBalanceVisibility] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [confirmPaymentOpen, setConfirmPaymentOpen] = useState(false);
 
   const toggleBalanceVisibility = () => {
     setBalanceVisibility(!balanceVisibility);
   };
+
+  const confirmPaymentHandler = () => {
+    setConfirmPaymentOpen(!confirmPaymentOpen);
+  }
 
   return (
     <div className="h-screen overflow-hidden">
@@ -45,6 +57,7 @@ export default function Home() {
             <h3 className="text-2xl font-extrabold text-slate-900 my-4">₦ 2,000,000</h3>
             <button
               type="button"
+              onClick={() => setOpen(true)}
               className="inline-flex items-center space-x-3 px-6 py-3 border border-indigo-400 text-indigo-600 shadow-sm text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               <span>Withdraw</span>
@@ -101,8 +114,160 @@ export default function Home() {
               generate payment address
             </button>
           </form>
+          {/* <div className="w-[300px] h-fit border border-indigo-700 min-w-fit">
+            QR code
+          </div> */}
         </div>
       </main>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <div className="fixed inset-0" />
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                      <div className="py-6 px-4 sm:px-6">
+                        <div className="flex items-center justify-end">
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="rounded-md bg-indigo-700 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+                              onClick={() => setOpen(false)}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <XIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="relative flex-1 py-6 px-4 sm:px-6">
+                        {/* Replace with your content */}
+                        <div className="absolute inset-0 py-6 px-4 sm:px-6">
+                          <h3 className="text-3xl text-slate-800 font-extrabold">Fill in Your Account Details</h3>
+                          <form action="#" method="POST" className="space-y-6 mt-10">
+                            <div>
+                              <label htmlFor="amount" className="block text-sm font-medium text-gray-700">
+                                Enter Amount to Withdraw
+                              </label>
+                              <div className="mt-1">
+                                <input
+                                  id="amount"
+                                  name="amount"
+                                  type="amount"
+                                  autoComplete="amount"
+                                  required
+                                  placeholder="eg. N 300"
+                                  className="appearance-none block w-full px-3 py-2 border border-gray-300 shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                              </div>
+                            </div>
+
+                            <div>
+                              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Recipient&apos;s Email Address
+                              </label>
+                              <div className="mt-1">
+                                <input
+                                  id="email"
+                                  name="email"
+                                  type="email"
+                                  autoComplete="email"
+                                  required
+                                  placeholder="STAY@gmail.com"
+                                  className="appearance-none block w-full px-3 py-2 border border-gray-300 shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                              </div>
+                            </div>
+
+                            <div>
+                              <label htmlFor="price" className="block text-sm font-medium text-gray-700">
+                                
+                              </label>
+                              <div className="mt-1 relative shadow-sm border border-gray-200">
+                                <div className="absolute inset-y-0 left-0 pl-3 pr-3 flex items-center pointer-events-none">
+                                  <LibraryIcon className="w-6 h-6 text-gray-500"/>
+                                </div>
+                                <select
+                                    id="currency"
+                                    name="currency"
+                                    className="focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 h-full py-2 w-full pl-10 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+                                  >
+                                    {banks.map(({id, name}) => (
+                                      <option key={id}>{name}</option>
+                                    ))}
+                                  </select>
+                              </div>
+                            </div>
+
+                            <div>
+                              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Recipient&apos;s Account Number
+                              </label>
+                              <div className="mt-1">
+                                <input
+                                  id="account-number"
+                                  name="account-number"
+                                  type="account-number"
+                                  autoComplete="account-number"
+                                  required
+                                  placeholder="123456789"
+                                  className="appearance-none block w-full px-3 py-2 border border-gray-300 shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                              </div>
+                            </div>
+
+                            <div>
+                              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                                Recipient&apos;s Account Name
+                              </label>
+                              <div className="mt-1">
+                                <input
+                                  id="name"
+                                  name="name"
+                                  type="name"
+                                  autoComplete="name"
+                                  required
+                                  placeholder="STAY TEAM"
+                                  className="appearance-none block w-full px-3 py-2 border border-gray-300 shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                />
+                              </div>
+                            </div>
+
+                            <div>
+                              <button
+                                type="submit"
+                                onClick={confirmPaymentHandler}
+                                className="flex justify-center uppercase px-16 py-2 border border-transparent text-md font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                              >
+                                withdraw
+                              </button>
+                            </div>
+                          </form>
+                        </div>
+                        {/* /End replace */}
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+      <OnBoarding />                       
+      {/* Confirm Payment Modal */}
+      <ConfirmPayment isOpen={confirmPaymentOpen} modalHandler={confirmPaymentHandler}/>
     </div>
   )
 }
